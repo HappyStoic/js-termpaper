49351ac18a1831a358b0664decba81710b9e76e7
Martin Řepa <repa.martiin@gmail.com>
2018-06-10 13:55:19 +0200

    Update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,8 @@ module.exports = function(grunt) {
                     style: 'compressed'
                 },
                 files: {
-                    'scss/dist/index.css' : 'scss/index.scss'
+                    'scss/dist/index.css' : 'scss/index.scss',
+                    'scss/dist/application.css' : 'scss/application/main.scss'
                 }
             }
         },
@@ -19,6 +20,15 @@ module.exports = function(grunt) {
                 },
                 src: ['js/index.js'],        // zdroj napsaný v ES 6
                 dest: 'js/dist/index.js'     // cílový soubor transpilovaný do ES 5.1
+            },
+            dist2: {
+                options: {
+                    transform: [['babelify', {presets: ['env']}]]
+                },
+                src: ['js/application/application.js', "js/application/apartementsControl.js",
+                "js/application/navigation.js", "js/application/map.js",
+                "js/application/loader.js", "js/application/todoList.js"],        // zdroj napsaný v ES 6
+                dest: 'js/dist/application.js'     // cílový soubor transpilovaný do ES 5.1
             }
         },
 
